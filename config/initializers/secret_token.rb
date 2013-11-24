71ec61d581ba7c921198772fb49bd680cb0eba00
Shannon Miller <slmiller99@gmail.com>
2013-11-24 14:20:47 -0500

    starting my rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp::Application.config.secret_key_base = '0b8da9eabf49b57541d7c55a35cec626f4fec957d72f60833c9295d80fb1d9d2129fc93a1c319d1d7659f1a1b7fc96b9944243a89a4d2b9278a03dc7bb671808'
